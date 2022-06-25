2f732b4fba6969684c4e323e1cadd1007922994c
Anthony Petrov <beavean@gmail.com>
2022-06-25 13:52:02 +0300

    Initial Commit

---
+++ Homework 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3
+//
+//  Created by Beavean on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
